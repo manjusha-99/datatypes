50d5acacd0551fbfa11a30aa99b9348eec15b4ad
manjusha-99 <146064294+manjusha-99@users.noreply.github.com>
2023-09-28 16:36:19 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,33 @@
+function add(){
+    var x="manjusha";
+    document.getElementById("label1").textContent=x;
+}
+function add1(){
+    let number=9898898888;
+    document.getElementById("label2").textContent=number;
+}
+function add2(){
+    const isTodayThursday=true;
+    document.getElementById("label3").textContent=isTodayThursday;
+}
+function add3(){
+    var fruits=["apple", "mango", "pineapple", "papaya", "custard apple"];
+    document.getElementById("label4").textContent=fruits;
+}
+function add4(){
+    let object={
+        name:"manju",
+     profession:"developer",
+    interested:"learning",
+    };
+    document.getElementById("label5").textContent=object.profession;
+    
+}
+function add5(){
+ var myNull="null the value inside is empty";
+    document.getElementById("label6").textContent=myNull;
+}
+function add6(){
+    let undefinedValue="undefined it means the value isn't declared yet";
+    document.getElementById("label7").textContent=undefinedValue;
+}
